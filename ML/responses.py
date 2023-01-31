b6a916b6f8e2dc31898271dbf57fb3dd34e7017d
Jdka1 <staryan.mehra@gmail.com>
2023-01-31 14:31:14 -0800

    webapp

---
+++ ML/responses.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Layers
+        self.m1 = nn.Linear(986, 986)
+        self.f1 = nn.LeakyReLU()
+        self.m2 = nn.Linear(986, 4096)
+        self.f2 = nn.LeakyReLU()
+        self.m3 = nn.Linear(4096, 1024)
+        self.f3 = nn.LeakyReLU()
+        self.m4 = nn.Linear(1024, 709)
+        self.f4 = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.m1(x)
+        x = self.f1(x)
+        x = self.m2(x)
+        x = self.f2(x)
+        x = self.m3(x)
+        x = self.f3(x)
+        x = self.m4(x)
+        x = self.f4(x)
+
+        return x
+
+
+class Bot:
+    def __init__(self):
+        self.model = self.load_model()
+        self.vectorizer = self.fit_vectorizer()
+        self.label_map = self.load_label_map()
+
+    def load_model(self):
+        model = Network()
+        model.load_state_dict(torch.load('ML/model_weights.pth'))
+        return model
+
+    def fit_vectorizer(self):
+        df = pd.read_csv('ML/data.csv')
+        train_df = df.drop(['example'], axis=1)
+        vectorizer = CountVectorizer()
+        vectorizer = vectorizer.fit(train_df['text'])
+        return vectorizer
+
+    def load_label_map(self):
+        df = pd.read_csv('ML/data.csv')
+        label_map = {i: j for (i, j) in zip(range(0, len(df)), df['label'])}
+        return label_map
+
+    def ask(self, query):
+        output = self.model(torch.FloatTensor(
+                self.vectorizer.transform([query]).toarray()
+            ))
+        pred_func = self.label_map[torch.argmax(output).item()]
+        return pred_func
+
+
+bot = Bot()
+print(bot.ask('hello'))
--- server/App.py
+++ server/App.py
@@ -1,4 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request
+import sys
+
+sys.path.insert(1, '/Users/arymehr/Documents/CS Projects/Numpy-Assistant/ML')
+from responses import Bot
+
 
 app = Flask(__name__)
 
@@ -6,9 +11,11 @@ app = Flask(__name__)
 def hello_world():
     if request.method == "POST":
         query = request.form["query"]
-        return render_template('index.html', response=query)
+        response = bot.ask(query)
+        return render_template('index.html', response=response)
     else:
         return render_template('index.html', response=None)
 
 if __name__ == '__main__':
+    bot = Bot()
     app.run(debug=True, port=8080, host='0.0.0.0')
